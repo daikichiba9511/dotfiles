252bcf9e23b1b8a305eea233efe84e255cb59fba
daikichiba9511 <daikichiba.hokkaido@gmail.com>
2022-01-03 16:20:00 +0900

    [neovim] add: install neovim cmd from source

---
+++ installer/install-nvim.sh
@@ -0,0 +1,22 @@
+#!/bin/sh 
+# pipeは使わない予定だけど一応
+set -eou pipefail
+
+echo $1
+
+# install neovim from source
+# vscode-neovimとの互換性を持たせるためにはver>=0.5が必要なのでソースからビルドする
+NVIM_DEFUALT_VER=0.6
+NVIM_VERSION=${1-$NVIM_DEFUALT_VER}
+# step:1 clone
+echo "✨ git cloned neovim"
+git clone https://github.com/neovim/neovim.git
+
+# step:2 build
+cd neovim
+git checkout "release-${NVIM_VERSION}}"
+sudo make CMAKE_BUILD_TYPE=Release
+sudo make install
+
+echo "✨ finished installing neovim"
+
