85d867403b2cbc02c6180c15c9ec6456acb01bda
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2022-01-13 11:51:55 +0900

    [pre] fix: installer-pre-ubuntu.sh

--- installer/install-pre-ubuntu.sh
+++ installer/install-pre-ubuntu.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -eu
 
 sudo apt-get install -y \
     gcc \
@@ -8,3 +9,10 @@ sudo apt-get install -y \
     git \
     vim \
     tmux \
+    bat \
+    fzf 
+
+# for bat
+# Ref: https://github.com/sharkdp/bat#on-ubuntu-using-apt
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
