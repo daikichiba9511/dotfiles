b61627879eac8dbf4287eb5134d3d4604f918d0f
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2022-01-27 12:41:06 +0900

    [nvim] fix: installer

--- installer/install-nvim.sh
+++ installer/install-nvim.sh
@@ -27,4 +27,6 @@ echo "✨ finished installing neovim"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+nvim +'PlugInstall --sync' +qa
+
 echo "✨ finished installing vim-plug"
