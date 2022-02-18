f40cfeda41ddee8d7d9cadcc09d869cd0f6ea64a
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2022-02-18 09:56:07 +0900

    [installer] fix: update installation of tool around zsh

--- installer/install-around-zsh.sh
+++ installer/install-around-zsh.sh
@@ -7,3 +7,8 @@ sudo apt-get install -y zsh powerline fonts-powerline
 # defaultのshellを変更
 # 次回以降のログイン時にshellを変える
 chsh -s $(which zsh)
+
+
+# zplug
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+
