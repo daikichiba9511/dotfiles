e12dca2dbb12eb445186566a40a9a5b0a8bfae3f
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2022-03-04 10:11:54 +0900

    [zsh] fix: update insaller of tools of zsh

--- installer/install-around-zsh.sh
+++ installer/install-around-zsh.sh
@@ -12,4 +12,5 @@ chsh -s $(which zsh)
 # zplug
 # curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 export ZPLUG_HOME=~/.zplug
+rm -rf ZPLUG_HOME
 git clone https://github.com/zplug/zplug $ZPLUG_HOME
