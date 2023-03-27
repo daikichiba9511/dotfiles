0676e6fc31a80973eb45ffb39c9035fab3bba322
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2023-03-27 17:36:18 +0900

    turn off settings calling vscodenotify directly

--- dotfiles/nvim/lua/plugins.lua
+++ dotfiles/nvim/lua/plugins.lua
@@ -1,7 +1,19 @@
 -- Reference:
 -- [1] https://github.com/yutkat/dotfiles/blob/main/.config/nvim/lua/rc/pluginlist.lua
+-- [2] https://github.com/milanglacier/nvim/blob/f54b7356dc97cbf9b07a77d5db1ad199c2ff3f2e/lua/conf/vscode.lua
 -- local install_path = vim.fn.stdpath("data") .. "/site/pack/jetpack/opt/vim-jetpack/plugin/jetpack.vim"
 -- local package_manager_url = "https://raw.githubusercontent.com/tani/vim-jetpack/master/plugin/jetpack.vim"
+--
+
+local keymap = vim.api.nvim_set_keymap
+
+local function notify(cmd)
+    return string.format("<cmd>call VSCodeNotify('%s')<CR>", cmd)
+end
+
+local function v_notify(cmd)
+    return string.format("<cmd>call VSCodeNotifyVisual('%s', 1)<CR>", cmd)
+end
 
 local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 local package_manager_url = "https://github.com/wbthomason/packer.nvim"
@@ -20,6 +32,29 @@ if vim.g.vscode then
     vim.fn.setenv("MACOSX_DEPLOYMENT_TARGET", "10.15")
     -- vim.cmd([[hi clear]])
     vim.opt.syntax = "off"
+    vim.filetype.add({
+        pattern = {
+            [".*%.ipynb"] = "python",
+        },
+    })
+    -- keymap("n", "<Leader>xr", notify("references-view.findReferences"), { silent = true }) -- language references
+    -- keymap("n", "<Leader>xd", notify("workbench.actions.view.problems"), { silent = true }) -- language diagnostics
+    -- keymap("n", "gr", notify("editor.action.goToReferences"), { silent = true })
+    -- keymap("n", "<Leader>rn", notify("editor.action.rename"), { silent = true })
+    -- keymap("n", "<Leader>fm", notify("editor.action.formatDocument"), { silent = true })
+    -- keymap("n", "<Leader>ca", notify("editor.action.refactor"), { silent = true })                -- language code actions
+
+    keymap("n", "<Leader>rg", notify("workbench.action.findInFiles"), { silent = true }) -- use ripgrep to search files
+    -- keymap("n", "<Leader>ts", notify("workbench.action.toggleSidebarVisibility"), { silent = true })
+    -- keymap("n", "<Leader>th", notify("workbench.action.toggleAuxiliaryBar"), { silent = true })   -- toggle docview (help page)
+    -- keymap("n", "<Leader>tp", notify("workbench.action.togglePanel"), { silent = true })
+    -- keymap("n", "<Leader>fc", notify("workbench.action.showCommands"), { silent = true })         -- find commands
+    -- keymap("n", "<Leader>ff", notify("workbench.action.quickOpen"), { silent = true })            -- find files
+    -- keymap("n", "<Leader>tw", notify("workbench.action.terminal.toggleTerminal"), { silent = true }) -- terminal window
+
+    -- keymap("v", "<Leader>fm", v_notify("editor.action.formatSelection"), { silent = true })
+    -- keymap("v", "<Leader>ca", v_notify("editor.action.refactor"), { silent = true })
+    -- keymap("v", "<Leader>fc", v_notify("workbench.action.showCommands"), { silent = true })
 else
     vim.opt.syntax = "enable"
     vim.fn.setenv("MACOSX_DEPLOYMENT_TARGET", "10.15")
