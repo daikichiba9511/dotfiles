8374e34c74d60fb890619b31c11a08e21be4d7c4
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2023-04-06 18:34:09 +0900

    fix: drop duplicates calling tasks/install-sheldon.sh as bug

--- setup.sh
+++ setup.sh
@@ -73,7 +73,6 @@ function parse_args() {
 
 function main() {
 	bash tasks/install-pre-ubuntu.sh
-    bash tasks/install-sheldon.sh
 	bash tasks/install-around-zsh.sh
 	bash tasks/install-sheldon.sh
 	bash tasks/install-nvim.sh
