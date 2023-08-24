ce6c66f08c19d6f28c3460ab6e23971d02b13990
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2023-08-24 13:06:56 +0900

    changes: update the way istalling nvim

--- tasks/install-nvim.sh
+++ tasks/install-nvim.sh
@@ -10,37 +10,54 @@ set -eu
 # NVIM_DEFUALT_VER=0.7
 # NVIM_VERSION=${1-$NVIM_DEFUALT_VER}
 # step:0 install prerequire
-sudo apt update && sudo apt upgrade
-sudo apt install -y \
-	ninja-build \
-	gettext \
-	libtool \
-	libtool-bin \
-	autoconf \
-	automake \
-	pkg-config \
-	doxygen \
-	make \
-	cmake
+# sudo apt update && sudo apt upgrade
+# sudo apt install -y \
+# 	ninja-build \
+# 	gettext \
+# 	libtool \
+# 	libtool-bin \
+# 	autoconf \
+# 	automake \
+# 	pkg-config \
+# 	doxygen \
+# 	make \
+# 	cmake
+#
+# # step:1 clone
+# echo "✨ git cloned neovim"
+# git clone https://github.com/neovim/neovim.git ~/neovim
+#
+# # step:2 build
+# cd ~/neovim
+# # master branchをビルド
+# # git checkout "release-${NVIM_VERSION}"
+# sudo make CMAKE_BUILD_TYPE=Release
+# sudo make install
+#
+# cd -
+# sudo rm -rf ~/neovim
+# echo "✨ finished installing neovim"
 
-# step:1 clone
-echo "✨ git cloned neovim"
-git clone https://github.com/neovim/neovim.git ~/neovim
+# Download stable version of neovim from github release for Linux
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -P /tmp
+# For destinaton directory, use /tmp
+mkdir -p ~/.local/bin/
+tar xzvf /tmp/nvim-linux64.tar.gz -C ~/.local/bin/
+sudo rm /tmp/nvim-linux64.tar.gz
+sudo rm /usr/local/bin/nvim
+sudo ln -s ~/.local/bin/nvim-linux64/bin/nvim /usr/local/bin/nvim
 
-# step:2 build
-cd ~/neovim
-# master branchをビルド
-# git checkout "release-${NVIM_VERSION}"
-sudo make CMAKE_BUILD_TYPE=Release
-sudo make install
+if [ ! -e /usr/local/bin/nvim ]; then
+	echo "######## ❌ Failed to install neovim?? (´・ω・｀) Plsease, check /usr/local/bin/nvim #########"
+	exit 1
+fi
 
-cd -
-sudo rm -rf ~/neovim
-echo "✨ finished installing neovim"
-
-# for jupyterlab
-curl -sL https://deb.nodesource.com/setup_19.x | bash -
-sudo apt upgrade -y && sudo apt install -yqq nodejs
+# for jupyterlab $ pyright $ copilot.vim
+echo "✨ installing nodejs for jupyterlab & pyright & copilot.vim"
+sudo apt update && sudo apt upgrade -yqq
+sudo apt autoremove
+curl -sL https://deb.nodesource.com/setup_19.x | sudo bash -
+sudo apt upgrade -yqq && sudo apt install -yqq nodejs
 
 # for nvim-treesitter
 # Ocasionaly, we cannot install tree-sitter by nvim-treesitter, so manually install these packages by npm
