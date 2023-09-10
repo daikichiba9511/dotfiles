5b0a737a64b9d52671a2b7740f35a5806cb2de02
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2023-09-10 15:57:43 +0900

    fix: image preview

--- dotfiles/nvim/lua/image_preview.lua
+++ dotfiles/nvim/lua/image_preview.lua
@@ -25,39 +25,39 @@
 ---@param directory string
 ---@return string[]
 local function find_images_in_dir(directory)
-	if vim.fn.executable("fd") == 0 then
-		error("fd is not installed or not executable. Please install fd and try again.")
-	end
+  if vim.fn.executable("fd") == 0 then
+    error("fd is not installed or not executable. Please install fd and try again.")
+  end
 
-	-- Make command to find image files
-	local wanted_extensions = { "jpg", "jpeg", "png" }
-	local command = "fd -e " .. table.concat(wanted_extensions, " -e ")
-	command = command .. " --type f --hidden --no-ignore-vcs --color=never . " .. directory
+  -- Make command to find image files
+  local wanted_extensions = { "jpg", "jpeg", "png" }
+  local command = "fd -e " .. table.concat(wanted_extensions, " -e ")
+  command = command .. " --type f --hidden --no-ignore-vcs --color=never . " .. directory
 
-	-- open with read mode
-	local handle = io.open(command, "r")
-	if handle == nil then
-		error("Failed to open fd output")
-	end
-	local result = handle:read("*a")
-	if result == nil then
-		error("Failed to read fd output")
-	end
-	handle:close()
+  -- open with read mode
+  local handle = io.open(command, "r")
+  if handle == nil then
+    error("Failed to open fd output")
+  end
+  local result = handle:read("*a")
+  if result == nil then
+    error("Failed to read fd output")
+  end
+  handle:close()
 
-	-- Get image paths
-	local image_paths = {}
-	for path in result:gmatch("[^\r\n]+") do
-		if is_file_in_the_directory(path, directory) then
-			table.insert(image_paths, path)
-		end
-	end
+  -- Get image paths
+  local image_paths = {}
+  for path in result:gmatch("[^\r\n]+") do
+    if is_file_in_the_directory(path, directory) then
+      table.insert(image_paths, path)
+    end
+  end
 
-	return image_paths
+  return image_paths
 end
 
 local function has_valid_extension(fp, ext)
-	return fp:match("^.+%." .. ext .. "$") ~= nil
+  return fp:match("^.+%." .. ext .. "$") ~= nil
 end
 
 ---@class ImagePreview
@@ -66,54 +66,54 @@ local M = {}
 --- Preview image in terminal using wezterm imgcat
 ---@param fp string
 M.ImagePreview = function(fp)
-	local absolte_path = vim.fn.expand(fp)
-	if absolte_path == nil then
-		error("Failed to get absolute path")
-	end
-	local wanted_extensions = { "jpg", "jpeg", "png" }
-	local is_valid_extension = false
-	for _, ext in ipairs(wanted_extensions) do
-		if has_valid_extension(absolte_path, ext) then
-			is_valid_extension = true
-			break
-		end
-	end
-	if not is_valid_extension then
-		error("Invalid extension")
-	end
+  local absolte_path = vim.fn.expand(fp)
+  if absolte_path == nil then
+    error("Failed to get absolute path")
+  end
+  local wanted_extensions = { "jpg", "jpeg", "png" }
+  local is_valid_extension = false
+  for _, ext in ipairs(wanted_extensions) do
+    if has_valid_extension(absolte_path, ext) then
+      is_valid_extension = true
+      break
+    end
+  end
+  if not is_valid_extension then
+    error("Invalid extension")
+  end
 
-	local command = {
-		"silent ",
-		"!wezterm cli split-pane ",
-		"-- ",
-		"bash ",
-		"-c ",
-		"'wezterm imgcat '" .. absolte_path .. "'; read'",
-	}
-	vim.api.nvim_command(table.concat(command, ""))
+  local command = {
+    "silent ",
+    "!wezterm cli split-pane ",
+    "-- ",
+    "bash ",
+    "-c ",
+    "'wezterm imgcat '" .. absolte_path .. "'; read'",
+  }
+  vim.api.nvim_command(table.concat(command, ""))
 end
 
 M.setup = function()
-	function WrappedImagePreview(fp)
-		M.ImagePreview(fp)
-	end
+  function WrappedImagePreview(fp)
+    M.ImagePreview(fp)
+  end
 
-	vim.api.nvim_create_user_command("ImgCat", "lua WrappedImagePreview(<q-args>)", {
-		nargs = 1,
-		-- complete = function(arg_lead, _, _)
-		-- 	local out = {}
-		-- 	local current_dir = vim.fn.getcwd()
-		-- 	if arg_lead == "" or current_dir == nil then
-		-- 		return out
-		-- 	end
-		-- 	local complete_list = find_images_in_dir(current_dir)
-		-- 	for _, v in ipairs(complete_list) do
-		-- 		table.insert(out, v)
-		-- 	end
-		-- 	return out
-		-- end,
-	})
-	-- vim.api.nvim_set_keymap("n", "<leader>ip", ":ImgCat<CR>", { noremap = true, silent = true })
+  vim.api.nvim_create_user_command("ImgCat", "lua WrappedImagePreview(<q-args>)", {
+    nargs = 1,
+    -- complete = function(arg_lead, _, _)
+    -- 	local out = {}
+    -- 	local current_dir = vim.fn.getcwd()
+    -- 	if arg_lead == "" or current_dir == nil then
+    -- 		return out
+    -- 	end
+    -- 	local complete_list = find_images_in_dir(current_dir)
+    -- 	for _, v in ipairs(complete_list) do
+    -- 		table.insert(out, v)
+    -- 	end
+    -- 	return out
+    -- end,
+  })
+  -- vim.api.nvim_set_keymap("n", "<leader>ip", ":ImgCat<CR>", { noremap = true, silent = true })
 end
 
 return M
