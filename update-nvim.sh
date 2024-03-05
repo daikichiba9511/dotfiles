5c775082c91b67f8cd2f979d6a5f4c4c7bd5163a
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2024-03-05 14:08:46 +0900

    update

--- update-nvim.sh
+++ update-nvim.sh
@@ -6,10 +6,21 @@
 sudo rm /usr/local/bin/nvim
 sudo rm -r /usr/local/share/nvim
 
-cd "/tmp"
-git clone https://github.com/neovim/neovim.git
-cd "/tmp/neovim"
-git checkout release-0.9
-make CMAKE_BUILD_TYPE=Release
-sudo make install
-rm -rf "/tmp/neovim"
+# Build neovim from source
+#
+# cd "/tmp"
+# git clone https://github.com/neovim/neovim.git
+# cd "/tmp/neovim"
+# git checkout release-0.9
+# make CMAKE_BUILD_TYPE=Release
+# sudo make install
+# rm -rf "/tmp/neovim"
+
+# Download stable version of neovim from github release for Linux
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -P /tmp
+# For destinaton directory, use /tmp
+mkdir -p ~/.local/bin/
+tar xzvf /tmp/nvim-linux64.tar.gz -C ~/.local/bin/
+sudo rm /tmp/nvim-linux64.tar.gz
+[ -e /usr/local/bin/nvim ] && sudo rm /usr/local/bin/nvim
+sudo ln -s ~/.local/bin/nvim-linux64/bin/nvim /usr/local/bin/nvim
