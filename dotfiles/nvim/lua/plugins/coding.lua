1b2eeb5fd45bb2b798a0d37acd39964be5ca316d
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2024-06-05 22:37:34 +0900

    update

--- dotfiles/nvim/lua/plugins/coding.lua
+++ dotfiles/nvim/lua/plugins/coding.lua
@@ -160,6 +160,14 @@ return {
       },
     },
   },
+  -- {
+  --   "MeanderingProgrammer/markdown.nvim",
+  --   name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+  --   dependencies = { "nvim-treesitter/nvim-treesitter" },
+  --   config = function()
+  --     require("render-markdown").setup({})
+  --   end,
+  -- },
   -- ToggleTerm
   -- {
   --   "akinsho/toggleterm.nvim",
