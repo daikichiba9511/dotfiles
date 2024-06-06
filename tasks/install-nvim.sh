5daec2d7092d5f168b4b9402ee2a49e775bb4560
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2024-06-06 20:04:04 +0900

    update

--- tasks/install-nvim.sh
+++ tasks/install-nvim.sh
@@ -60,7 +60,7 @@ echo "✨ installing nodejs for jupyterlab & pyright & copilot.vim"
 sudo apt update
 sudo apt install -y ca-certificates curl gnupg
 sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=20
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 sudo apt update
