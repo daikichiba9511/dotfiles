5c7923863f8ba5a873421cc601226cfbc489f8fc
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2024-06-06 19:59:22 +0900

    update

--- tasks/install-around-zsh.sh
+++ tasks/install-around-zsh.sh
@@ -12,7 +12,7 @@ sudo chsh "${USER}" -s "$(which zsh)"
 # zplug
 # curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 export ZPLUG_HOME="${HOME}/.zplug"
-rm -rf ZPLUG_HOME
+rm -rf "${ZPLUG_HOME}"
 git clone https://github.com/zplug/zplug "${ZPLUG_HOME}"
 
 # starship
