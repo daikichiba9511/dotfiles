a0954aa30e6785a3a3957b7951736a7f334952ab
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2024-10-24 21:18:50 +0900

    update

--- setup.sh
+++ setup.sh
@@ -361,7 +361,7 @@ function main() {
 
     install_package powerline
     install_package fonts-powerline
-    install_package python3.9-venv # for ruff
+    install_package python3-venv # for ruff
 
   elif [[ "${OS_TYPE}" = 'Mac' ]]; then
     arch -arm64 brew install \
