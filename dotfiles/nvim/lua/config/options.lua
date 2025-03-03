43f939221a93b1238d960cc62d534ac5ec965b50
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2025-03-03 17:11:35 +0900

    update

--- dotfiles/nvim/lua/config/options.lua
+++ dotfiles/nvim/lua/config/options.lua
@@ -1,3 +1,46 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+local keymap = vim.api.nvim_set_keymap
+
+vim.opt.shell = "zsh"
+vim.opt.clipboard = "unnamedplus"
+-- vim.opt.spelllang = { "en", "cjk" }
+vim.opt.spelllang = {}
+vim.opt.list = true
+-- opt.listchars = { tab = "»»", space = "·" }
+-- opt.listchars = { tab = "»»" }
+vim.opt.listchars = "tab:»»"
+vim.opt.cmdheight = 0
+vim.opt.conceallevel = 0
+
+keymap("n", "<C-x>", "<Cmd>lua require('bufdelete').bufwipeout(0, true)<CR>", { noremap = true, silent = true })
+
+local function quick_chat()
+  local input = vim.fn.input("Quick chat: ")
+  if input ~= "" then
+    require("CopilotChat").ask(input, { selection = require("CopilotChat.select").buffer })
+  end
+end
+
+vim.keymap.set("n", "<leader>ccq", quick_chat, { noremap = true, silent = true, desc = "CopilotChat - Quick chat" })
+
+-- Toggleterm {{{
+function _G.set_terminal_keymaps()
+  local opts = { noremap = true }
+  vim.api.nvim_buf_set_keymap(0, "t", "<esc>", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-h>", [[<C-\><C-n><C-W>h]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-j>", [[<C-\><C-n><C-W>j]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-k>", [[<C-\><C-n><C-W>k]], opts)
+  vim.api.nvim_buf_set_keymap(0, "t", "<C-l>", [[<C-\><C-n><C-W>l]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+-- vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+-- vim.cmd('autocmd! TermEnter term://*toggleterm#* tnoremap <silent><c-t> <Cmd> exe v:count1 . "ToggleTerm"<CR>')
+
+vim.api.nvim_set_keymap("n", "<leader>tt", [[<Cmd>exe v:count1 . "ToggleTerm"<CR>]], { noremap = true, silent = true })
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+-- }}}
