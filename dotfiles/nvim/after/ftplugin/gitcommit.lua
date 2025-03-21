2eeff8f79b5bb826a700dd758e051807a10b9865
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2025-03-21 22:52:13 +0900

    feat(nvim): add CopilotChat for git commit assistance
    - Enable CopilotChat.nvim plugin with claude-3.7-sonnet model
    - Add ftplugin for gitcommit that automatically opens CopilotChat
    - Configure keymaps for accessing commit assistant and proper cleanup
    - Use chat_autocomplete for better interaction during commits

---
+++ dotfiles/nvim/after/ftplugin/gitcommit.lua
@@ -0,0 +1,16 @@
+local function lazy_has(plugin_name)
+  return require("lazy.core.config").spec.plugins[plugin_name] ~= nil
+end
+
+--- Reference
+--- 1. https://zenn.dev/vim_jp/articles/041300c0f9cc50
+local commit_bufnr = vim.api.nvim_get_current_buf()
+if lazy_has("CopilotChat.nvim") then
+  vim.keymap.set("n", "<leader>ga", "<cmd>CopilotChatCommit<CR>", { buffer = commit_bufnr })
+  vim.schedule(function()
+    require("CopilotChat")
+    vim.cmd.CopilotChatCommit()
+  end)
+  vim.api.nvim_create_autocmd("QuitPre", { command = "CopilotChatClose" })
+  vim.keymap.set("ca", "qq", "execute 'CopilotChatClose' <bar> wqa")
+end
