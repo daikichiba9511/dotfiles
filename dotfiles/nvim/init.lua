156e6992b9e291ca62c587a55345565210467957
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2025-08-28 10:43:14 +0900

    update

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -585,6 +585,7 @@ require("mason-lspconfig").setup({
   automatic_installation = true,
   ensure_installed = ls_names,
 })
+
 vim.lsp.enable(ls_names)
 
 vim.lsp.config("*", {
@@ -603,37 +604,6 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {},
 })
-
-local nmap = function(keys, func, desc)
-  if desc then
-    desc = "LSP: " .. desc
-  end
-  vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-end
-nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
-
-nmap("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-nmap("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-nmap("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-nmap("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-
-nmap("K", vim.lsp.buf.hover, "Hover Documentation")
-
-nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-
-nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-nmap("<leader>wl", function()
-  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-end, "[W]orkspace [L]ist Folders")
-
--- vim.api.nvim_buf_create_user_command(bufnr, "format", function(_)
---   vim.lsp.buf.format()
--- end, { desc = "format current buffer with lsp" })
 
 -- Definer User Commands
 -- :Configでinit.luaを開く
