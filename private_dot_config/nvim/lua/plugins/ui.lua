e0a53158c3d0ab9d59e72c87741165bc59a028d4
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2026-02-09 17:12:22 +0900

    refactor(nvim): remove bufferline plugin
    Use Snacks.picker.buffers() (<leader>,) for buffer
    selection instead of persistent bufferline tab bar.

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -218,16 +218,6 @@ return {
     end,
   },
 
-  -- Bufferline
-  {
-    "akinsho/bufferline.nvim",
-    version = "*",
-    event = "VeryLazy",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("bufferline").setup()
-    end,
-  },
 
   -- Noice - UI for messages, cmdline and popupmenu
   {
