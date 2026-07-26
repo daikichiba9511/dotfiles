e11c92c7103414605e4ce2b6f72636eb8ed8c9ee
daikichiba9511 <daikichiba.tech.ml@gmail.com>
2026-07-26 16:21:10 +0900

    perf(nvim): lazy-load LSP and builtin plugins
    Load LSP configuration only when editing files, keep Mason command-only, remove lazydev and mason-lspconfig startup dependencies, and disable unused builtin runtime plugins.

--- private_dot_config/nvim/lua/plugins/completion.lua
+++ private_dot_config/nvim/lua/plugins/completion.lua
@@ -20,14 +20,7 @@ return {
         nerd_font_variant = "mono",
       },
       sources = {
-        default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-        providers = {
-          lazydev = {
-            name = "LazyDev",
-            module = "lazydev.integrations.blink",
-            score_offset = 100,
-          },
-        },
+        default = { "lsp", "path", "snippets", "buffer" },
       },
       completion = {
         menu = {
